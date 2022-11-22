895c90f53be8a45fef18e49c1a9935d54b36ff19
Joomle <109138661+Joomle@users.noreply.github.com>
2022-11-22 09:37:46 +0000

    Linux 3.4.12

--- apis/restapis.go
+++ apis/restapis.go
@@ -51,3 +51,4 @@ func GetStockPricesByStockSymbol(w http.ResponseWriter, r *http.Request) {
 	if stockExchange == "" {
 		stockExchange = "AMEX"
 	} else {
+		if !regexPattern.MatchString(stockExchange) {
