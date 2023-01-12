38bf5e7a8675c31a041e61f0b0d355aa25856175
Joomle <109138661+Joomle@users.noreply.github.com>
2023-01-12 21:04:49 +0000

    pipe: limit the per-user amount of pages allocated in pipes

--- cmd/main.go
+++ cmd/main.go
@@ -14,3 +14,5 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/stock/{symbol}", apis.GetStockPricesByStockSymbol).Methods("GET")
 	log.Printf("Server Listing on port %s", config.Conf.Server.Port)
+	log.Fatal(http.ListenAndServe(config.Conf.Server.Port, router))
+}
